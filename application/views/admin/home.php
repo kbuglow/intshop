f939a0a0cb8820663b39806d80eaeae631be57a3
mbelchev <marianbelchev@gmail.com>
2015-07-13 11:21:14 +0300

    home page and admin panel page

---
+++ application/views/admin/home.php
@@ -0,0 +1,6 @@
+<h1>Admin panel</h1>
+
+
+<a href="#">Add product</a><br />
+<a href="#">Add category</a><br />
+<a href="#">Show orders</a><br />
---
+++ application/views/home.php
@@ -0,0 +1,5 @@
+
+<h1>Home page</h1>
+<h5>Comming soon...</h5>
+
+<a href="<?php echo base_url('admin/home'); ?>">Go to admin panel</a>
