36231a6d87a9ed87b34cd93965692ffe79232bf4
mbelchev <marianbelchev@gmail.com>
2015-07-14 15:57:51 +0300

    DB

--- intshop.sql
+++ intshop.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 14, 2015 at 02:36 PM
+-- Generation Time: Jul 14, 2015 at 02:57 PM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -102,7 +102,7 @@ CREATE TABLE IF NOT EXISTS `photos` (
 `id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
   `url` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `photos`
@@ -129,7 +129,7 @@ CREATE TABLE IF NOT EXISTS `products` (
   `new_price` decimal(10,2) NOT NULL,
   `in_stock` int(11) NOT NULL,
   `main_photo` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `products`
@@ -218,12 +218,12 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 -- AUTO_INCREMENT for table `photos`
 --
 ALTER TABLE `photos`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `users`
 --
