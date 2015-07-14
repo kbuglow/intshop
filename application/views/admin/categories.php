c6a660a048fbb88659da307c960d13a697e2d082
Radoslav Kostadinov <radoslav.j.kostadinov@gmail.com>
2015-07-14 13:01:28 +0300

    categories - Add parent and add children

--- application/views/admin/categories.php
+++ application/views/admin/categories.php
@@ -1,18 +1,26 @@
 <html>
 <head>
-    <meta charset="utf-8" />
+    <meta charset="utf-8"/>
 </head>
 <body>
-<h1>Categories  | <a id="status">Read</a></h1>
+<h1>Categories | <a id="status">Read</a></h1>
+<input type="button" onclick="location.href='<?php echo base_url('admin'); ?>'" value="Back"/>
 <button id="edit_toggle">Change Mode</button>
-<input type="button" onclick="location.href='<?php echo base_url('admin');?>'" value="Back" />
+<button id="toggle_all">Toggle All</button>
+
+
 <?php
-echo form_open('admin/category/edit_category');
+echo form_open('admin/category/handle_request');
 ?>
 <ul>
-<?php echo $categories; ?>
+    <?php echo $categories; ?>
 </ul>
-
+<?php
+echo form_close();
+echo form_open('admin/category/add_category');
+?>
+<input type="text" name="new_cat">
+<input type="submit" value="Add new category"/>
 <?php
 echo form_close();
 ?>
@@ -20,37 +28,46 @@ echo form_close();
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script>
-        $(document).ready(function(){
-            $('.cat').click(function() {
-                var id = $(this).attr('id');
-                $('#sub' + id).slideToggle("fast");
-             });
-            $('#edit_toggle').click(function() {
-                if($('#status').html() == "Read") {
-                    $('#status').html("Edit");
-                    $('.options').slideToggle();
-                } else{
-                    $('#status').html("Read");
-                    $('.options').slideToggle();
-
-                }
-            });
-            $('.edit_btn').click(function(){
-                var id = $(this).attr('id');
-                var number = id.split("-")[1];
-                var t = $('#' + number).text();
-                $('#' + number).html($('<input name="new_name" value="'+t+'"/>'));
-                $(this).replaceWith(' <input type="hidden" name="cat_id" value="' +number+ '"><input type="submit" value="Edit">');
-
-            });
-            $('.delete_btn').each(function(){
-                var id = $(this).attr('id');
-                var number = id.split("-")[1];
-                $(this).attr("href", "<?php echo base_url('admin/category/delete_category')?>/" + number);
-
-
-            });
+    $(document).ready(function () {
+        $('.cat').click(function () {
+            $('#sub' + $(this).attr('id')).slideToggle("fast");
         });
+        $('#edit_toggle').click(function () {
+            if ($('#status').html() == "Read") {
+                $('#status').html("Edit");
+                $('.options').slideToggle();
+            } else {
+                $('#status').html("Read");
+                $('.options').slideToggle();
+
+            }
+        });
+        $('.edit_btn').click(function () {
+            var id = $(this).attr('id');
+            var number = id.split("-")[1];
+            var t = $('#' + number).text();
+            $('#' + number).html($('<input name="new_name" value="' + t + '"/>'));
+            $(this).replaceWith('<input type="hidden" name="cat_id" value="' + number + '"><input type="submit" name="submit_edit" value="Edit"/>');
+
+        });
+        $('.delete_btn').each(function () {
+            var id = $(this).attr('id');
+            var number = id.split("-")[1];
+            $(this).attr("href", "<?php echo base_url('admin/category/delete_category')?>/" + number);
+
+
+        });
+        $('#toggle_all').click(function () {
+            $('.children').slideToggle("fast");
+        });
+        $('.add_btn').click(function () {
+            var id = $(this).attr('id');
+            var number = id.split("-")[1];
+            $("#list_element-" + number).after('<input type="text"  name="new_cat"/><input type="submit" name="submit_add" value="Add"/>' +
+                '<input type="hidden" name="parent_id" value="' + number + '">');
+        });
+
+    });
 </script>
 </body>
 </html>
