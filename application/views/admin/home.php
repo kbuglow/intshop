f2eb258f9963601c867495c3ad44e5e88b9bb5ff
mbelchev <marianbelchev@gmail.com>
2015-07-14 11:24:59 +0300

    users

--- application/views/admin/home.php
+++ application/views/admin/home.php
@@ -3,4 +3,5 @@
 <a href="#">Add product</a><br />
 <a href="<?php echo base_url('admin/category'); ?>">Add category</a><br />
 <a href="<?php echo base_url('admin/orders'); ?>">Show orders</a><br />
+<a href="#">Users</a><br />
 <a href="<?php echo base_url('admin/logout'); ?>">Logout</a><br />
