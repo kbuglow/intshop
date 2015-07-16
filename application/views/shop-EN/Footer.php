2c49973dd4427ef3947843a803ccb7a2f209a1a8
Veselin Dechev <veskopos@mail.bg>
2015-07-16 15:05:03 +0300

    EN version

---
+++ application/views/shop-EN/Footer.php
@@ -0,0 +1,59 @@
+</body>
+
+<footer>
+
+    <div id="footer-content">
+        <div id="address">
+            <p>Address:</p>
+            <p>Wien, ?sterreich</p>
+            <p>tel: +43 650 5856228  </p>
+            <div id="social-networks-profiles">
+                <a href="#" id="facebook"><img/></a>
+                <a href="#" id="google-plus"><img/></a>
+                <a href="#" id="pinterest"><img/></a>
+                <a href="#" id="linked-in"><img/></a>
+                <a href="#" id="twitter"><img/></a>
+                <a href="#" id="instagram"><img/></a>
+            </div>
+        </div>
+
+        <div id="contact-us">
+            <p>Contact us:</p>
+
+            <div id="send-email">
+                <p>Send a mail</p>
+            </div>
+        </div>
+
+        <table id="more-options">
+            <tr>
+                <td id="term-of-use">
+                    Terms of Use
+                </td>
+                <td id="delivery">
+                    Delivery
+                </td>
+            </tr>
+            <tr>
+                <td id="third-row">
+                    Payment
+                </td>
+                <td id="payment">
+                    <div id="pay-options">
+                        <div id="paypal-logo"></div>
+                        <div id="visa-logo"></div>
+                        <div id="mastercard-logo"></div>
+                        <div id="sofort-logo"></div>
+                    </div>
+                </td>
+            </tr>
+        </table>
+    </div>
+
+    <hr id="footer-horizontal-line" />
+
+    <div id="copyright"></div>
+    <div id="move-to-top"></div>
+</footer>
+<script src="ProductListContent/ShowAddToCardOption.js"></script>
+</html>
