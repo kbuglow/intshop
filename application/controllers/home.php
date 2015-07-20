fa468de07d4ae1d9c6ae0efcb6c7239fcae6c012
mbelchev <marianbelchev@gmail.com>
2015-07-20 12:02:44 +0300

    fix logged in user info

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -4,8 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Home extends CI_Controller {
 
 	public function index() {
-		$this->load->model('admin/Users_model');
-
 		$data = array();
 		
 		if (is_logged_in()) $data['user'] = $this->Users_model->get_user($this->session->userdata('user_id'));
