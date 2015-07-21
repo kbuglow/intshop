9b08c29cdea90e39c848c43acd0525a482e3b599
nobody.petkov@gmail.com <1haven0pa55>
2015-07-21 13:38:50 +0300

    change navigation position

--- application/views/shop/news_home.php
+++ application/views/shop/news_home.php
@@ -8,9 +8,9 @@
     <fieldset>
         <legend id="fieldset-title">News</legend>
         <?php foreach ($news as $new): ?>
-            <div>
-                <h1 class="news_header"><?php echo $new->title;?></h1>
-                <h4>posted by <?php echo $new->username; ?> on <?php echo $new->date;?></h4>
+            <div class="news">
+                <h1><?php echo $new->title;?></h1>
+                <h2>posted by <?php echo $new->username; ?> on <?php echo $new->date;?></h2>
 
                 <p>
                     <?php echo $new->text;?>
