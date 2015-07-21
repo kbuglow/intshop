a6d1cdc14b68ecedc27582e84e5c53c9926c29db
nobody.petkov@gmail.com <1haven0pa55>
2015-07-21 10:04:46 +0300

    fixed category method

--- assets/js/ShowProducts.js
+++ assets/js/ShowProducts.js
@@ -1,18 +1,19 @@
 function changePathName() {
-    var currentPath = document.getElementById('current-path-paragraph').innerHTML,
-        newPart,
-        indexOfCategory,
-        pathParts = currentPath.split(' &gt; ');
+    var indexOfCategory = window.location.href.slice(window.location.href.lastIndexOf('/') + 1, window.location.href.length);
 
+    if (parseInt(indexOfCategory) >= 0) {
+        var currentPath = document.getElementById('current-path-paragraph').innerHTML,
+            newPart,
+            pathParts = currentPath.split(' &gt; ');
 
-    indexOfCategory = window.location.href.slice(window.location.href.lastIndexOf('/') + 1, window.location.href.length);
-    console.log(indexOfCategory);
-    document.getElementById(indexOfCategory).setAttribute('title', 'current');
-    newPart = document.getElementById(indexOfCategory).firstElementChild.innerHTML;
+        document.getElementById(indexOfCategory).setAttribute('title', 'current');
+        newPart = document.getElementById(indexOfCategory).firstElementChild.innerHTML;
 
-    pathParts[pathParts.length - 1] = newPart;
+        pathParts[pathParts.length - 1] = newPart;
 
-    document.getElementById('current-path-paragraph').innerHTML = pathParts.join(' &gt; ');
+        document.getElementById('current-path-paragraph').innerHTML = pathParts.join(' &gt; ');
+    }
 }
+
 
 window.onload = changePathName;
