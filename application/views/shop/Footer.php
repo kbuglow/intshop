8031a0e7a7bb940100451b0e6ad9586ea41525c4
nobody.petkov@gmail.com <1haven0pa55>
2015-07-22 15:52:07 +0300

    change settings styles

--- application/views/shop/Footer.php
+++ application/views/shop/Footer.php
@@ -63,6 +63,5 @@
 <script src="<?php echo base_url('assets/js'); ?>/MoveToTop.js"></script>
 <script src="<?php echo base_url('assets/js'); ?>/PaginationOfProducts.js"></script>
 <script src="<?php echo base_url('assets/js'); ?>/AddLeafToNavigation.js"></script>
-<script src="<?php echo base_url('assets/js'); ?>/MainPageSliderFunctionality.js"></script>
 <script src="<?php echo base_url('assets/js'); ?>/ism-2.0.1-min.js"></script>
 </html>
